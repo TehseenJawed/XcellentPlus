faa0b6ec03334d938ab89e2a61be8daf5595876a
TEHSEEN JAWED <tehseenjawed1@gmail.com>
2021-07-11 19:56:17 +0500

    Displaying my well structure code

--- src/components/websiteFlow/WebsiteFlow.jsx
+++ src/components/websiteFlow/WebsiteFlow.jsx
@@ -2,13 +2,15 @@ import React, {useState} from 'react'
 import './WebsiteFlow.scss'
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/Home/Logo.svg'
-
 import {FiLogIn} from 'react-icons/fi'
 import {FaRegUser} from 'react-icons/fa'
-
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Popper from '../popper/Popper'
+
+// Mobile Header is here
+import MobileFlow from '../mobileFlow/MobileFlow'
+import SeriveTab from '../../components/servicesTab/servicesTab'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -16,15 +18,19 @@ const useStyles = makeStyles((theme) => ({
         margin: theme.spacing(1),
       },
     },
+    desktopMenu:{},
     quoteBtn:{
         color:'white',
         backgroundColor:'#5f45ea',
-        width:'100%',
+        width:200,
         fontSize:10,
         fontWeight:300,
         transition:'0.3s',
         marginLeft:5,
         boxShadow:'0px 5px 10px #999999',
+        [theme.breakpoints.down('md')]: {
+            width:300,
+          },
         "&:hover, &:focus":{
             backgroundColor:'#7962f5',
             boxShadow:'0px 0px 10px gray',
@@ -37,6 +43,12 @@ function WebsiteFlow({ children }) {
     const classes = useStyles();
     const [anchorEl, setAnchorEl] = useState(null);
     const [popperText, setPopperText] = useState("")
+    const [openServices, setOpenServices] = useState(null);
+
+    const manageService = () => {
+        setOpenServices(true)
+    }
+    
     
     return (
         <div>
@@ -46,38 +58,39 @@ function WebsiteFlow({ children }) {
 
                 <div className="header-items">
 
-                    <div className="header-item">
+                    <div className="header-item" onMouseOver={() => setOpenServices(false)}>
                         <Link className="header-link" style={{ color: '#5f45ea' }}>
                             Home
                         </Link>
                     </div>
 
-                    <div className="header-item">
+                    <div className="header-item" onMouseOver={manageService}>
                         <Link className="header-link">
-                            About
+                            Services
                         </Link>
                     </div>
 
-                    <div className="header-item">
+                    <div className="header-item" onMouseOver={() => console.log(false)}>
                         <Link className="header-link">
-                            Blog
+                            Packages
                         </Link>
                     </div>
 
-                    <div className="header-item">
+                    <div className="header-item" onMouseOver={() => setOpenServices(false)}>
                         <Link className="header-link">
-                            Shop
+                            Technologies
                         </Link>
                     </div>
 
-                    <div className="header-item">
+                    <div className="header-item" onMouseOver={() => setOpenServices(false)}>
                         <Link className="header-link">
-                            Contact
+                            About Us
                         </Link>
                     </div>
-
+ 
                 </div>
-                <div className="header-items">
+
+                <div className="header-items-response">
 
                     <div className="login-btn">
                         <FaRegUser 
@@ -94,11 +107,10 @@ function WebsiteFlow({ children }) {
                            />
                     </div>
                         
- 
                     <Button 
                     variant="contained" 
                     className={classes.quoteBtn}
-                    onMouseOver={(event) => {setAnchorEl(anchorEl ? null : event.currentTarget); setPopperText("Build You own custom Package and you will be paying for what you have chosen.")}} 
+                    onMouseOver={(event) => {setAnchorEl(anchorEl ? null : event.currentTarget); setPopperText("Build your own custom Package and you will be paying for what you have chosen.")}} 
                     onMouseOut={(event) => setAnchorEl(anchorEl ? null : event.currentTarget)}
                     >
                         Create Your Package
@@ -107,8 +119,15 @@ function WebsiteFlow({ children }) {
                 </div>
 
             </div>
+
+            <SeriveTab open={openServices} setOpen={setOpenServices}/>
+
+            <MobileFlow />
+
             {/* Website Header End */}
+
             {children}
+            
         </div>
     )
 }
